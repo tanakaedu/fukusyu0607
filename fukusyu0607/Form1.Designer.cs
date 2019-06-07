c75b3812858ea7937351efbd30a4a0da05a08856
tanakaedu <tanaka@am1.jp>
2019-06-07 14:30:43 +0900

    速度乱数

--- fukusyu0607/Form1.Designer.cs
+++ fukusyu0607/Form1.Designer.cs
@@ -28,20 +28,57 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
-			// 
+			//
+			// label1
+			//
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(643, 220);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(131, 33);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "(▼皿▼)";
+			//
+			// timer1
+			//
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 20;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			//
+			// label2
+			//
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label2.Location = new System.Drawing.Point(12, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(146, 33);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Time: 100";
+			//
 			// Form1
-			// 
+			//
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(423, 427);
+			this.ClientSize = new System.Drawing.Size(730, 576);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,9 +12,64 @@ namespace fukusyu0607
 {
 	public partial class Form1 : Form
 	{
+		// 疑似乱数
+		// ランダムのシード(種)を指定して初期化したら、
+		// それを使い続ける。
+		private static Random rand = new Random();
+		int vx = rand.Next(-10, 11);
+		int vy = rand.Next(-10, 11);
+		int time = 100*5;
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			// 以下に、label1.Leftとlabel1.Topの座標をランダムで求めよ
+
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			label1.Left += vx;
+			label1.Top += vy;
+
+			// マウスと重なった時、タイマーを止める
+			Point p = PointToClient(MousePosition);
+
+			if (	(p.X >= label1.Left)
+				&&	(p.X < label1.Right)
+				&&	(p.Y >= label1.Top)
+				&&	(p.Y < label1.Bottom)
+				)
+			{
+				timer1.Enabled = false;
+			}
+
+			// 左で跳ね仮り(Math.Abs(vx)で、vxの絶対値)
+			if (label1.Left < 0)
+			{
+				vx = Math.Abs(vx);
+			}
+			// 上で跳ね返り
+			if (label1.Top < 0)
+			{
+				vy = Math.Abs(vy);
+			}
+			// 右で跳ね仮り
+			//// フォームの右端 = ClientSize.Width
+			//// ラベルの右端 = label1.Right
+			if (label1.Right > ClientSize.Width)
+			{
+				vx = -Math.Abs(vx);
+			}
+			// 下で跳ね仮り
+			if (label1.Bottom > ClientSize.Height)
+			{
+				vy = -Math.Abs(vy);
+			}
+
+			time--;
+			label2.Text = "Time: " + time;
 		}
 	}
 }
